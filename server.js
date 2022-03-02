905ca5d121685ee936c2ef61ea3a94d8dae9c647
Ted McGinness <ttmcginness22@gmail.com>
2022-03-02 12:43:08 -0600

    added engines

--- server.js
+++ server.js
@@ -7,11 +7,8 @@ const db = mongoose.connection;
 const PROJECT3_DB = process.env.PROJECT3_DB
 const PORT = process.env.PORT || 3003;
 
-
 app.use(express.json())
 app.use(cors())
-
-
 
 // Connect to Mongo
 mongoose.connect(PROJECT3_DB, { useNewUrlParser: true });
